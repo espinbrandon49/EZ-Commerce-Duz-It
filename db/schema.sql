159ab6a136c8b1e6d5e57033ae74d50f160b315e
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-01 16:50:17 -0700

    fix typos in readmes

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
-
--- The walkthrough video must demonstrate how to create the schema from the MySQL shell.
-
--- //1) The walkthrough video must demonstrate how to create the schema from the MySQL shell.
--- //2) The walkthrough video must demonstrate how to seed the database from the command line.
--- //3) The walkthrough video must demonstrate how to start the application’s server.
