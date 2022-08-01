ecf522758e8df7b3b183f25fb6e2574ff0a8fed6
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-01 16:48:21 -0700

    delete comments

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
--- server.js
+++ server.js
@@ -13,9 +13,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server DONE
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+  app.listen(PORT, () => console.log(`Server listening on: http://localhost:${PORT}`));
 });
-
-//1) The walkthrough video must demonstrate how to create the schema from the MySQL shell.
-//2) The walkthrough video must demonstrate how to seed the database from the command line.
-//3) The walkthrough video must demonstrate how to start the application’s server.
