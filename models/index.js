9d150ffbc079e82ad924070d58f02efdd1c4324b
Brandon Espinosa <espinbrandon49@gmail.com>
2022-08-09 16:46:40 -0700

    fixed model associations

--- models/index.js
+++ models/index.js
@@ -16,12 +16,14 @@ Category.hasMany(Product, {
 
 // Product belongToMany Tag (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'product_id',
 })
 
 // Tag belongToMany Product (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag
+  through: ProductTag,
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
