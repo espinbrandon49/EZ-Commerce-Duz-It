2c51578e6ce8bcadfd9a373abd1b3b81f3161449
Brandon Espinosa <espinbrandon49@gmail.com>
2022-09-12 19:35:49 -0700

    render all categories to the browser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const cors = require('cors')
 // import sequelize connection DONE
 
 const app = express();
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use(routes);
 
