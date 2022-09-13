91dbbf4d7af517b6ad991d90b8c017864eccee0b
Brandon Espinosa <espinbrandon49@gmail.com>
2022-09-12 17:25:20 -0700

    add client folder and run create-react-app . inside of client folder

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import axios from 'axios'
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    axios.get('')
+  }, [])
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
